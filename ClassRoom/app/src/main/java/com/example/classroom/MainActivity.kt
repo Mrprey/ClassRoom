df16ef5af257cdbc04e634f51b4d3bfdde0cd3e2
Mrprey <prey.anderson2@gmail.com>
2022-01-31 15:10:55 -0300

    Action bar disabled

--- ClassRoom/app/src/main/java/com/example/classroom/MainActivity.kt
+++ ClassRoom/app/src/main/java/com/example/classroom/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.classroom
 
 import android.os.Bundle
+import android.view.Menu
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -22,18 +24,24 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         //setando a toolbar
-
+        setSupportActionBar(findViewById(R.id.toolbar))
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
+/*        val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_mural, R.id.navigation_arquivos, R.id.navigation_atividades,
                 R.id.navigation_pessoas
             )
         )
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        setupActionBarWithNavController(navController, appBarConfiguration)*/
         navView.setupWithNavController(navController)
     }
+/*    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.bottom_nav_menu, menu)
+        return true
+    }*/
+
 }
