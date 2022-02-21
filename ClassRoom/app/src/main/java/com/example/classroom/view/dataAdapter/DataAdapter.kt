d8412f6cd7cef8f86f037d0e578736df2d2fd823
Mrprey <prey.anderson2@gmail.com>
2022-02-21 13:02:57 -0300

    Novas telas construidas

--- ClassRoom/app/src/main/java/com/example/classroom/view/dataAdapter/DataAdapter.kt
+++ ClassRoom/app/src/main/java/com/example/classroom/view/dataAdapter/DataAdapter.kt
@@ -4,14 +4,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.classroom.R
 
 class DataAdapter(private val data: MutableList<String>) : RecyclerView.Adapter<DataAdapter.DataViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_list, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_list_atividades, parent, false)
         return DataViewHolder(view)
     }
 
@@ -24,7 +23,7 @@ class DataAdapter(private val data: MutableList<String>) : RecyclerView.Adapter<
     inner class DataViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         //var cardView = this.itemView.findViewById<CardView>(R.id.cv_Titulo)
         fun bind(example: String) {
-            var textView_title = this.itemView.findViewById<TextView>(R.id.tv_textTitulo)
+            var textView_title = this.itemView.findViewById<TextView>(R.id.tv_textTitle)
             textView_title.text = example
             //set ImageView people
            // var imageView_status = this.itemView.findViewById<ImageView>(R.id.iv_status)
